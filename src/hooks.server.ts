a8c634b1045b4792fc09a2012f108710fd102bea
Alex Bjørlig <alexbjorlig@icloud.com>
2024-09-12 12:09:11 +0200

    small update

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -13,37 +13,14 @@ Sentry.init({
 
     return null;
   },
-
-  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
-  // spotlight: import.meta.env.DEV,
 });
 
 // If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
 export const handle = sequence(sentryHandle());
 
-// If you have a custom error handler, pass it to `handleErrorWithSentry`
-
-const myErrorHandler: HandleServerError = ({
-  error: handledError,
-  event,
-  status,
-  message,
-}) => {
-  let errId = "";
-
-  const errMessage = `${
-    (handledError as any)?.message || "handleError - no error message!"
-  } (@handleError)`;
-
-  if (status < 500) {
-    console.log('Error is less than 500, so we are not sending it to Sentry');
-    return;
-  }
-  Sentry.setContext("sveltekit", { event });
-
-  errId = Sentry.captureException(handledError);
-
-  console.log("🔥", errMessage, { handledError, errId });
+const myErrorHandler: HandleServerError = ({}) => {
+  console.log('Lets ignore error - i.e. we are NOT reporting');
+  return;
 };
 
 export const handleError = Sentry.handleErrorWithSentry(myErrorHandler);
